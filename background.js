6927f3908ffe79ab95dbc9afa217fee046587a15
Paradox-Incarnation <paradoxincarnation@gmail.com>
2026-01-29 12:02:35 +0530

    Uploaded Extension Files

---
+++ background.js
@@ -0,0 +1,47 @@
+var background = function() {
+  "use strict";
+  var _a, _b;
+  function defineBackground(arg) {
+    if (arg == null || typeof arg === "function") return { main: arg };
+    return arg;
+  }
+  const browser$1 = ((_b = (_a = globalThis.browser) == null ? void 0 : _a.runtime) == null ? void 0 : _b.id) ? globalThis.browser : globalThis.chrome;
+  const browser = browser$1;
+  const definition = defineBackground(() => {
+    browser.runtime.onInstalled.addListener(async ({ reason }) => {
+      if (reason !== "install") return;
+      await browser.tabs.create({
+        url: browser.runtime.getURL("/teach.html"),
+        active: true
+      });
+    });
+  });
+  background;
+  function initPlugins() {
+  }
+  function print(method, ...args) {
+    return;
+  }
+  const logger = {
+    debug: (...args) => print(console.debug, ...args),
+    log: (...args) => print(console.log, ...args),
+    warn: (...args) => print(console.warn, ...args),
+    error: (...args) => print(console.error, ...args)
+  };
+  let result;
+  try {
+    initPlugins();
+    result = definition.main();
+    if (result instanceof Promise) {
+      console.warn(
+        "The background's main() function return a promise, but it must be synchronous"
+      );
+    }
+  } catch (err) {
+    logger.error("The background crashed on startup!");
+    throw err;
+  }
+  const result$1 = result;
+  return result$1;
+}();
+background;
